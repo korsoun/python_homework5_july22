b1bb06e19eb8a6ecdc7326c29c8f7164feeff0b1
alexkors <korsun55555@mail.ru>
2022-07-23 18:16:37 +0700

    Удалить из текста слова с абв

---
+++ 1.py
@@ -0,0 +1,22 @@
+def istheretext (string, part_string):
+    thereis = False
+    for i in range(len(string)-len(part_string)+1):
+        if string[i:i+len(part_string)] == part_string:
+            thereis = True
+    return thereis
+
+def delete_words (text, part_of_text):
+    find = istheretext
+    text_list = text.split()
+    new_text_list = []
+    for word in text_list:
+        if not find(word, part_of_text):
+            new_text_list.append(word)
+    new_text = ''
+    for word in new_text_list:
+        new_text += f' {word}'
+    return new_text
+
+text = 'авог нро ваа абв абвег дштдла пкпк ертабв'
+str = 'абв'
+print(delete_words(text, str))
